55a03f5dc9b5021bf92f6e6c8bbe13431ed4643f
matyurin <urinm1504@gmail.com>
2025-01-11 14:03:27 +0200

    Add files via upload

--- jbvjh.py
+++ jbvjh.py
@@ -1,6 +1,21 @@
+import random
 
-age = int(input("Введіть ваш вік: "))
-if age > 18:
-    print("Вхід дозволено!")
+secret_number = random.randint(1, 10)
+
+attempts = 3
+
+print("Гра 'Вгадай число'!")
+print("Програма загадала число від 1 до 10. У вас є 3 спроби, щоб його вгадати.")
+
+for i in range(attempts):
+    guess = int(input(f"Спроба {i + 1}. Введіть ваше число: "))
+
+    if guess == secret_number:
+        print("Вітаємо! Ви вгадали число!")
+        break
+    elif guess > secret_number:
+        print("Менше!")
+    else:
+        print("Більше!")
 else:
-    print("Вхід заборонено!")
+    print(f"Ви програли! Загадане число було {secret_number}.")
